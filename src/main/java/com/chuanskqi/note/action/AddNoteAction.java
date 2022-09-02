38efac1639e90f6b2a9227e15b8b7f34b64add6c
shenkang2 <shenkang2@gome.com>
2022-09-02 18:54:56 +0800

    保留最新一次选择的目录，优先显示

--- src/main/java/com/chuanskqi/note/action/AddNoteAction.java
+++ src/main/java/com/chuanskqi/note/action/AddNoteAction.java
@@ -11,6 +11,7 @@ import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.LogicalPosition;
 import com.intellij.openapi.ui.Messages;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
@@ -21,6 +22,9 @@ import org.jetbrains.annotations.NotNull;
  */
 public class AddNoteAction extends AnAction {
     private static final Logger LOG = Logger.getInstance(AddNoteAction.class);
+
+    // 保留最新一次选择的目录
+    private String lastSelectCategory = "";
 
     @Override
     public void update(@NotNull AnActionEvent e) {
@@ -52,12 +56,16 @@ public class AddNoteAction extends AnAction {
             .distinct()
             .collect(Collectors.toList());
 
+        // 让最近一次选择的目录优先显示
+        Collections.sort(allCategory, (o1, o2) -> lastSelectCategory.equals(o1) ? -1 : 0);
+
         String selectCode = editor.getDocument().getText().split("\n")[logicalPos.line].trim();
         String category = Messages.showEditableChooseDialog(selectCode,
             "add note", Messages.getInformationIcon(),
             allCategory.toArray(new String[allCategory.size()]), allCategory.get(0), null);
 
         if (StringUtils.isNotBlank(category)) {
+            lastSelectCategory = category;
             String alias = Messages.showInputDialog(fileName, "add alias", Messages.getInformationIcon());
             Note newNote = Note.builder()
                 .fileName(fileName)
